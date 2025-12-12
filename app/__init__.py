eb2f50d12f0c339ca7e13e16d1643b195cb7d1bd
RudrakshaRavindra <ravindra.rudraksha@thrymr.net-at-175029991794>
2025-12-12 16:00:18 +0530

    Changes done in run.py  file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,31 @@
 from flask import Flask
+from flasgger import Swagger
 from app.routes.image_routes import image_bp
+from app.routes.filters_routes import filters_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(image_bp, url_prefix="/image")
+
+    # Swagger config
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": 'apispec_1',
+                "route": '/apispec_1.json',
+                "rule_filter": lambda rule: True,  # include all routes
+                "model_filter": lambda tag: True,
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/docs/"
+    }
+
+    Swagger(app, config=swagger_config)
+
+    # Register routes
+    app.register_blueprint(image_bp, url_prefix="/api")
+    app.register_blueprint(filters_bp, url_prefix="/api")
+
     return app
